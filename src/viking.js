6550744c2b615c54ab85a8bb2491cf4a070fdcbd
yungmanny26 <tondreau24@gmail.com>
2022-02-05 09:29:52 -0500

    solved lab

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,49 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength){
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(DamageTaken){
+    this.health -= DamageTaken;
+  }
+}
 
 // Viking
-class Viking {}
+  class viking extends Soldier{
+    constructor(name, health, strength){
+      super(health, strength);
+      this.name = name;
+    }
+    receiveDamage(amount){
+      super.receiveDamage(amount);
+      if (this.health > 0) {
+        return `${this.name} has received ${amount} points of damage`;
+      } else { 
+        return `${this.name} has died in the act of combat`;
+      }
+   
+    }
+    battleCry(){
+      return 'Odin Owns You All!';
+    }
+  }
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+  receiveDamage(amount) {
+    super.receiveDamage(amount)
+    if (this.health > 0){
+      return `A Saxon has received ${amount} points of damage`
+    } else {
+      return ` A Saxon had died in combat`
+    }
+  }
+}
 
 // War
 class War {}
